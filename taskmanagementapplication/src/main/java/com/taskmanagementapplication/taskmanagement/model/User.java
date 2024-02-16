9c12bcbf2674102b980af21b2240154d3b573844
FaaizN <fzndm1@gmail.com>
2024-02-15 22:25:05 -0600

    Seventh Commit for BackEnd Applications of Software Architecture for Big Data - Added Integration tests for taskmangementapplication.taskmanagement repositories - Added application-test.properties for integration test to make sure that database and application are communicating effectively - Added Unit test for DataCollector Package - Added Unit test for DataAnalyzer Package

--- taskmanagementapplication/src/main/java/com/taskmanagementapplication/taskmanagement/model/User.java
+++ taskmanagementapplication/src/main/java/com/taskmanagementapplication/taskmanagement/model/User.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 
 import java.util.List;
 
-@Entity
+@Entity(name = "user_table")
 public class User {
 
     // Set ID as primary key
--- taskmanagementapplication/src/main/java/com/taskmanagementapplication/taskmanagement/repository/UserRepository.java
+++ taskmanagementapplication/src/main/java/com/taskmanagementapplication/taskmanagement/repository/UserRepository.java
@@ -5,11 +5,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
-    @Query("SELECT CASE WHEN COUNT(u) > 0 THEN true ELSE false END FROM User u WHERE u.userName = :userName")
+    @Query("SELECT CASE WHEN COUNT(u) > 0 THEN true ELSE false END FROM user_table u WHERE u.userName = :userName")
     boolean existsByUserName(String userName);
+
+    List<User> findAll();
 
     User findByUserName(String userName);
 }
